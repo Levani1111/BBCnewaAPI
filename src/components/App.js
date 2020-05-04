7624f29b878787dc9d86118c30f049a6e5f8e22a
Levani Papashvili <papashvililevani@gmail.com>
2020-05-04 11:53:09 -0400

    BBC News Api

---
+++ src/components/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import News from './News/News';
+
+import './App.css';
+
+class App extends Component  {
+  constructor(props) {
+    super(props);
+    this.state = {
+      news1: {
+        type: "top-headlines",
+        query: "surces=bbc-news",
+      },
+      // news2: {
+      //   type: "everything",
+      //   query: "domains=wsj.com,nytimes.com"
+      // },
+    };
+  }
+  render() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">BBC News </h1>
+      </header>
+      <News news={this.state.news1} />
+      {/* <News news={this.state.news2} /> */}
+    </div>
+  );
+}
+}
+
+export default App;
---
+++ src/components/News/News.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import NewSingle from './NewSingle';
+
+class News extends Component {
+    constructor(props) {
+        super(props);
+         this.state = {
+            news: [],
+        };
+    }
+    componentDidMount() {
+    const url = 
+  
+    'https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=ed71b36e32e541b6af7691438d3b68d8';
+    
+      fetch(url)
+       .then((response) => {
+           return response.json();
+      
+      })
+      .then((data) => {
+          this.setState({
+              news: data.articles
+          })
+      })
+      .catch((error) => console.log(error));
+    }
+
+    renderItems() {
+        return this.state.news.map((item) => (
+            <NewSingle key={item.url} item={item} />
+        ));
+    }
+
+
+    render() {
+        return (
+            <div className="row">
+                {this.renderItems()}
+            </div>
+        );
+    }
+}
+
+export default News;
