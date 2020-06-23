10f6beeaadd5e62d5c50ce7d8e9186da6b731768
Levani Papashvili <papashvililevani@gmail.com>
2020-06-23 13:05:07 -0400

    update build file

---
+++ build/precache-manifest.3de5578e141b29d8ec1934aaa560eed9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d3d05e675e7454ebc23090320e2ee2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48d7e2dffe4f6182e50b",
+    "url": "/static/css/main.570cfa8d.chunk.css"
+  },
+  {
+    "revision": "b0913273cf8878a04adc",
+    "url": "/static/js/2.88bc1f8c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.88bc1f8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48d7e2dffe4f6182e50b",
+    "url": "/static/js/main.026bd7f0.chunk.js"
+  },
+  {
+    "revision": "4f7dd83a465c46d2be72",
+    "url": "/static/js/runtime-main.293bbabf.js"
+  }
+]);
