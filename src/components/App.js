0bb59693bfca82ee34c762c12efcfcea4087639c
Levani Papashvili <papashvililevani@gmail.com>
2020-07-23 13:08:07 -0400

    update

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import News from './News/News';
 import "@patternfly/react-core/dist/styles/base.css";
 
+
 import './App.css';
 
 class App extends Component  {
